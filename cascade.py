115bcec1fcd1d128d688be74019ea1dbc3bbc84a
Bekhzodbek <91641488+McAlan23@users.noreply.github.com>
2022-10-24 11:36:47 +0900

    Add files via upload

---
+++ cascade.py
@@ -0,0 +1,24 @@
+import cv2 as cv
+
+cap = cv.VideoCapture(0)
+
+face_cascade = cv.CascadeClassifier('cascade.xml')
+
+while (True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Our operations on the frame come here
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+    for (x, y, w, h) in faces:
+        cv.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+    cv.imshow('frame', frame)
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv.destroyAllWindows()
